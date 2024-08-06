88a10f98428c95eacdd1a6e6f9b890fc6923014f
ucybers <90274068+UCYBERS@users.noreply.github.com>
2024-08-06 22:49:20 +0300

    setupkali.sh

--- setupkali.sh
+++ setupkali.sh
@@ -11,6 +11,7 @@
 # revision var
 revision="1.0.0"
 
+
 RED='\033[31m'
 GREEN='\033[32m'
 YELLOW='\033[33m'
@@ -20,28 +21,38 @@ CYAN='\033[36m'
 WHITE='\033[37m'
 RESET='\033[0m' 
 
+
 if ! grep -q "Kali" /etc/os-release; then
     echo -e "${RED}This script is intended to be run on Kali Linux only.${RESET}"
     exit 1
 fi
+
 
 asciiart=$(base64 -d <<< "H4sICP9gsmYAA2xvZ28udHh0AH1OMQ4CMQzb+wqPTJcPoA6c+ACIAclSJcTNIBaE1McTJ1cECxnq
 xLFTAz/VGvCHmTTpKVqoEgzoalfSm+7MmJBTb60XWDjNpGbcZ+wZSkppMMrF1edPiT3IdH94IL6u
 qIq31TiutPBZBqhM8AjKkScgcMYNm5SF2iKnivYNWTLcb8/lsVxjcXkJTvN5tz8ch6i8ASj+YXlX
 AQAA" | gunzip)
 
+
 echo -e "$asciiart"
 
+
 echo
+
 
 change_to_gnome() {
     echo -e "${BLUE}Updating system and installing GNOME...${RESET}"
     sudo apt update -y
-    sudo apt install -y kali-desktop-gnome
-    echo -e "${BLUE}Setting GNOME as default session...${RESET}"
-    echo "1" | sudo update-alternatives --config x-session-manager
+    sudo apt install -y kali-desktop-gnome gdm3
+    echo -e "${BLUE}Setting GNOME and gdm3 as default session...${RESET}"
+    
+    sudo update-alternatives --set x-session-manager /usr/bin/gnome-session
+    
+    sudo systemctl enable gdm3
+    sudo systemctl start gdm3
     sudo apt purge --autoremove -y kali-desktop-xfce
 }
+
 
 enable_root_login() {
     echo -e "${BLUE}Allowing root login in GDM...${RESET}"
@@ -52,17 +63,20 @@ enable_root_login() {
     echo -e "${GREEN}Root login enabled and root password set to 'ucybers'.${RESET}"
 }
 
+
 install_tools_for_root() {
     echo -e "${BLUE}Installing tools for root user...${RESET}"
     sudo apt update -y
     sudo apt install -y terminator leafpad mousepad
 }
 
+
 setup_all() {
     change_to_gnome
     enable_root_login
     install_tools_for_root
 }
+
 
 show_menu() {
     clear
@@ -77,6 +91,7 @@ show_menu() {
     echo -e " ${BLUE}0 - Exit                      (Exit the script)${RESET}\n"
     read -n1 -p "  Press key for menu item selection or press X to exit: " menuinput
 
+    
     echo
 
     case $menuinput in
@@ -89,14 +104,18 @@ show_menu() {
     esac
 }
 
+
 show_menu
+
 
 clear
 echo -e "$asciiart"
 echo -e "\n${RED}Happy Hacking!${RESET}"
 echo -e "${GREEN}Setup completed! Please type 'reboot' to apply the changes and restart the system.${RESET}"
 
+
 read -p "Type 'reboot' to restart: " user_input
+
 
 if [ "$user_input" == "reboot" ]; then
     sudo reboot
