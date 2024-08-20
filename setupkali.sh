4f12da9af0fa680bdf2d395dfaa317f10548f31c
ucybers <90274068+UCYBERS@users.noreply.github.com>
2024-08-20 16:54:35 +0300

    setupkali.sh

--- setupkali.sh
+++ setupkali.sh
@@ -22,6 +22,7 @@ WHITE='\033[37m'
 BOLD='\033[1m'
 RESET='\033[0m' 
 greenplus='\e[1;33m[++]\e[0m'
+greenminus='\e[1;33m[--]\e[0m'
 
 finduser="root"
 force=0
@@ -117,27 +118,25 @@ change_background() {
     echo -e "\n  ${GREEN}Background changed to ${BACKGROUND_IMAGE}${RESET}"
 }
 
-install_wifi_hotspot() {
-    echo "Installing dependencies..."
-    sudo apt update
-    DEBIAN_FRONTEND=noninteractive sudo apt install -y libgtk-3-dev hostapd libqrencode-dev libpng-dev pkg-config
-
-    echo "Cloning and installing linux-wifi-hotspot..."
-    
-    # Clone and install linux-wifi-hotspot as root
-    if [ -d "/opt/linux-wifi-hotspot" ]; then
-        echo "linux-wifi-hotspot directory already exists."
-    else
-        sudo -u root git clone https://github.com/lakinduakash/linux-wifi-hotspot /opt/linux-wifi-hotspot
-        sudo -u root bash -c "cd /opt/linux-wifi-hotspot && make && sudo make install"
-        
-        # Remove the directory after installation
-        echo "Removing linux-wifi-hotspot directory..."
-        sudo -u root rm -rf /opt/linux-wifi-hotspot
+fix_sources() {
+    check_space=$(cat /etc/apt/sources.list | grep -c "# deb-src http://.*/kali kali-rolling.*")
+    check_nospace=$(cat /etc/apt/sources.list | grep -c "#deb-src http://.*/kali kali-rolling.*")
+    get_current_mirror=$(cat /etc/apt/sources.list | grep "deb-src http://.*/kali kali-rolling.*" | cut -d "/" -f3)
+    if [[ $check_space = 0 && $check_nospace = 0 ]]; then
+    	echo -e "\n  $greenminus # deb-src or #deb-sec not found - skipping"
+    elif [ $check_space = 1 ]; then
+      echo -e "\n  $greenplus # deb-src with space found in sources.list uncommenting and enabling deb-src"
+      # relaxed sed
+      sudo sed 's/\# deb-src http\:\/\/.*\/kali kali-rolling.*/\deb-src http\:\/\/'$get_current_mirror'\/kali kali-rolling main contrib non\-free''/' -i /etc/apt/sources.list
+      echo -e "\n  $greenplus new /etc/apt/sources.list written with deb-src enabled"
+    elif [ $check_nospace = 1 ]; then
+      echo -e "\n  $greenplus #deb-src without space found in sources.list uncommenting and enabling deb-src"
+      # relaxed sed
+      sudo sed 's/\#deb-src http\:\/\/.*\/kali kali-rolling.*/\deb-src http\:\/\/'$get_current_mirror'\/kali kali-rolling main contrib non\-free''/' -i /etc/apt/sources.list
+      echo -e "\n  $greenplus new /etc/apt/sources.list written with deb-src enabled"
     fi
-    
-    echo "linux-wifi-hotspot has been successfully installed. You can now run the tool as the kali user."
-}
+    sudo sed -i 's/non-free$/non-free non-free-firmware/' /etc/apt/sources.list
+    }
 
 
 setup_all() {
@@ -148,7 +147,7 @@ setup_all() {
     configure_dash_apps
     install_icons
     change_background
-    install_wifi_hotspot
+    fix_sources
     
 }
 
