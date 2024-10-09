f76c9950deb16eb8d26e74d6f5ba6fa4288fa4f4
ucybers <90274068+UCYBERS@users.noreply.github.com>
2024-10-09 23:18:07 +0300

    Update setupkali.sh

--- setupkali.sh
+++ setupkali.sh
@@ -69,12 +69,21 @@ install_icons() {
 
 change_to_gnome() {
     echo -e "${BLUE}Updating system and installing GNOME...${RESET}"
+    
     sudo apt update -y
+    
     sudo apt install -y kali-desktop-gnome
+    
     echo -e "${BLUE}Setting GNOME as default session...${RESET}"
-   
-    echo "1" | sudo update-alternatives --config x-session-manager
-    sudo apt purge --autoremove -y kali-desktop-xfce
+    
+    sudo update-alternatives --set x-session-manager /usr/bin/gnome-session
+    
+    echo -e "${BLUE}Removing XFCE...${RESET}"
+    sudo apt purge --autoremove -y kali-desktop-xfce xfce4*
+    
+    sudo apt autoremove -y
+    
+    echo -e "${GREEN}GNOME has been set as the default environment and XFCE has been removed.${RESET}"
 }
 
 
